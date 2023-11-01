ee16cdc1e553c3599c34d5d84da807aee82c0259
Leofred Selga Nuñez <143275696+devilyor25@users.noreply.github.com>
2023-11-01 18:50:40 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-var typed = new Typed('#element', {
-    strings: ['<i>Computer</i> Technian.', 'Computer Science 3rd Year Student'],
-    typeSpeed: 50,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-  });
-
-  
